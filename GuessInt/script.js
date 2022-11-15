a09bfee5aa9bbbea61874784864c571c103617c0
Alex Sanmartín <isanmartin8a@gmail.com>
2022-11-15 10:13:18 +0100

    avanzamos

--- GuessInt/script.js
+++ GuessInt/script.js
@@ -1,1 +1,15 @@
-'use strict';
+/* eslint-disable no-const-assign */
+/* eslint-disable no-unused-vars */
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+const score = 20
+const highscore = 0
+
+const scoreField = document.querySelector('.score').textContent
+const highscoreField = document.querySelector('.highscore').textContent
+const numberFile = document.querySelector('.guess').textContent
+
+scoreField = score
+highscoreField = highscore
+numberFile = secretNumber
+
+console.log(`El campo score tiene el valor ${scoreField}`)
